1b35d8cfd9156bb618b8373844c68a2bcea33ae0
Ashkan Daie <ashkan.daie@gmail.com>
2018-03-05 10:42:48 -0800

    linux is case sensative

--- src/MediaInfo.DotNetWrapper/NativeMethods.cs
+++ src/MediaInfo.DotNetWrapper/NativeMethods.cs
@@ -6,56 +6,56 @@ namespace MediaInfo.DotNetWrapper
     internal static class NativeMethods
     {
         // Import of DLL functions. DO NOT USE until you know what you do (MediaInfo DLL do NOT use CoTaskMemAlloc to allocate memory)
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfo_New();
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern void MediaInfo_Delete(IntPtr handle);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfo_Open(IntPtr handle, [MarshalAs(UnmanagedType.LPWStr)] string fileName);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfoA_Open(IntPtr handle, IntPtr fileName);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfo_Open_Buffer_Init(IntPtr handle, long fileSize, long fileOffset);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfoA_Open(IntPtr handle, long fileSize, long fileOffset);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfo_Open_Buffer_Continue(IntPtr handle, IntPtr buffer, IntPtr bufferSize);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfoA_Open_Buffer_Continue(
           IntPtr handle,
           long fileSize,
           byte[] buffer,
           IntPtr bufferSize);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern long MediaInfo_Open_Buffer_Continue_GoTo_Get(IntPtr handle);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern long MediaInfoA_Open_Buffer_Continue_GoTo_Get(IntPtr handle);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfo_Open_Buffer_Finalize(IntPtr handle);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfoA_Open_Buffer_Finalize(IntPtr handle);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern void MediaInfo_Close(IntPtr handle);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfo_Inform(IntPtr handle, IntPtr reserved);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfoA_Inform(IntPtr handle, IntPtr reserved);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfo_GetI(
           IntPtr handle,
           IntPtr streamKind,
@@ -63,7 +63,7 @@ namespace MediaInfo.DotNetWrapper
           IntPtr parameter,
           IntPtr kindOfInfo);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfoA_GetI(
           IntPtr handle,
           IntPtr streamKind,
@@ -71,7 +71,7 @@ namespace MediaInfo.DotNetWrapper
           IntPtr parameter,
           IntPtr kindOfInfo);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfo_Get(
           IntPtr handle,
           IntPtr streamKind,
@@ -80,7 +80,7 @@ namespace MediaInfo.DotNetWrapper
           IntPtr kindOfInfo,
           IntPtr kindOfSearch);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfoA_Get(
           IntPtr handle,
           IntPtr streamKind,
@@ -89,40 +89,40 @@ namespace MediaInfo.DotNetWrapper
           IntPtr kindOfInfo,
           IntPtr kindOfSearch);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfo_Option(
           IntPtr handle,
           [MarshalAs(UnmanagedType.LPWStr)] string option,
           [MarshalAs(UnmanagedType.LPWStr)] string value);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfoA_Option(IntPtr handle, IntPtr option, IntPtr value);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfo_State_Get(IntPtr handle);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfo_Count_Get(IntPtr handle, IntPtr streamKind, IntPtr streamNumber);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfoList_New();
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern void MediaInfoList_Delete(IntPtr handle);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfoList_Open(
           IntPtr handle,
           [MarshalAs(UnmanagedType.LPWStr)] string fileName,
           IntPtr options);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern void MediaInfoList_Close(IntPtr handle, IntPtr filePos);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfoList_Inform(IntPtr handle, IntPtr filePos, IntPtr reserved);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfoList_GetI(
           IntPtr handle,
           IntPtr filePos,
@@ -131,7 +131,7 @@ namespace MediaInfo.DotNetWrapper
           IntPtr parameter,
           IntPtr kindOfInfo);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfoList_Get(
           IntPtr handle,
           IntPtr filePos,
@@ -141,16 +141,16 @@ namespace MediaInfo.DotNetWrapper
           IntPtr kindOfInfo,
           IntPtr kindOfSearch);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfoList_Option(
           IntPtr handle,
           [MarshalAs(UnmanagedType.LPWStr)] string option,
           [MarshalAs(UnmanagedType.LPWStr)] string value);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfoList_State_Get(IntPtr handle);
 
-        [DllImport("MediaInfo")]
+        [DllImport("mediainfo")]
         internal static extern IntPtr MediaInfoList_Count_Get(
           IntPtr handle,
           IntPtr filePos,
